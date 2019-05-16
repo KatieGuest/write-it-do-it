18cfae67fa7a31e2e59302d7d610846d4778272a
Katie Guest <katieguest11@gmail.com>
2019-05-16 15:20:11 -0400

    end of challenge 1

--- challenge-1/router.js
+++ challenge-1/router.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const express = require('express');
-const {Dog} = require('../app/server/models');
+const { Dog } = require('../app/server/models');
 
 // This router is already mounted on `/api` in app/server/app.js
 const router = express.Router();
-
+router.post('/dogs', async (req, res, next) => {
+  const all = await Dog.create(req.body);
+  const message = {
+    message: 'Success',
+    data: all
+  };
+  console.log('ALL', all);
+  res.status(201).send(message);
+});
 
 module.exports = router;
--- challenge-1/router.spec.js
+++ challenge-1/router.spec.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-unused-expressions */
-"use strict";
+'use strict';
 
 // Assertions
-const chai = require("chai");
+const chai = require('chai');
 const expect = chai.expect;
-const chaiThings = require("chai-things");
+const chaiThings = require('chai-things');
 chai.use(chaiThings);
 
 // Model
-const { Dog } = require("../app/server/models");
+const { Dog } = require('../app/server/models');
 
 // Routes
-const app = require("../app/server/app");
-const agent = require("supertest")(app);
+const app = require('../app/server/app');
+const agent = require('supertest')(app);
 
-describe("Challenge 1 - Express route", () => {
+describe('Challenge 1 - Express route', () => {
   // ------------------------------------
   // ----------- TEST SETUP -------------
   // ------------------------------------
   const dogData = {
-    name: "Cody",
+    name: 'Cody',
     age: 5
   };
 
@@ -28,31 +28,31 @@ describe("Challenge 1 - Express route", () => {
   // ------------------------------------
   // you can use `response` and `dogFromDb`
   // to help make assertions!
-  describe("POST /api/dogs", () => {
-    xit("response status should be 201", async () => {
-      const response = await agent.post("/api/dogs").send(dogData);
+  describe('POST /api/dogs', () => {
+    it('response status should be 201', async () => {
+      const response = await agent.post('/api/dogs').send(dogData);
 
       // your expect assertion here...
       expect(response.status).to.equal(201);
     });
 
-    xit("response should be an object with a message and data field", async () => {
-      const response = await agent.post("/api/dogs").send(dogData);
+    it('response should be an object with a message and data field', async () => {
+      const response = await agent.post('/api/dogs').send(dogData);
 
       // your expect assertion here...
       expect(response.body.message).to.exist;
       expect(response.body.data).to.exist;
     });
 
-    xit("message field of response should be `Success`", async () => {
-      const response = await agent.post("/api/dogs").send(dogData);
+    it('message field of response should be `Success`', async () => {
+      const response = await agent.post('/api/dogs').send(dogData);
 
       // your expect assertion here...
-      expect(response.body.message).to.equal("Success");
+      expect(response.body.message).to.equal('Success');
     });
 
-    xit("response data should match the db data", async () => {
-      const response = await agent.post("/api/dogs").send(dogData);
+    it('response data should match the db data', async () => {
+      const response = await agent.post('/api/dogs').send(dogData);
       const dogFromDb = await agent.get(`/api/dogs/${response.body.data.id}`);
 
       // your expect assertion here...
